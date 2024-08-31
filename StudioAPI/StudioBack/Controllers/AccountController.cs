2a3439a2a8b9c94d329c7e68b00fa9b7640c67e8
ArielC27 <109997773+ArielC27@users.noreply.github.com>
2024-08-30 21:23:14 -0300

    Add Logout

--- StudioAPI/StudioBack/Controllers/AccountController.cs
+++ StudioAPI/StudioBack/Controllers/AccountController.cs
@@ -6,81 +6,90 @@ using StudioService.LoginService;
 
 namespace StudioBack.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class AccountController : ControllerBase
-    {
-        private readonly IAccountService _accountService;
-        private readonly IMapper _mapper;
+	[ApiController]
+	[Route("[controller]")]
+	public class AccountController : ControllerBase
+	{
+		private readonly IAccountService _accountService;
+		private readonly IMapper _mapper;
 
-        public AccountController(IAccountService loginService, IMapper mapper)
-        {
-            _accountService = loginService;
-            _mapper = mapper;
-        }
+		public AccountController(IAccountService loginService, IMapper mapper)
+		{
+			_accountService = loginService;
+			_mapper = mapper;
+		}
 
-        [HttpPost("Login")]
-        public async Task<IActionResult> Login([FromBody] UserLoginDto userLoginDto)
-        {
-            var loginResult = await _accountService.Login(userLoginDto);
+		[HttpPost("Login")]
+		public async Task<IActionResult> Login([FromBody] UserLoginDto userLoginDto)
+		{
+			var loginResult = await _accountService.Login(userLoginDto);
 
-            if (loginResult != null)
-            {
-                return Ok(loginResult);
-            }
+			if (loginResult != null)
+			{
+				return Ok(loginResult);
+			}
 
-            return Unauthorized("Invalid username or password");
+			return Unauthorized("Invalid username or password");
 
-        }
-        
-        [HttpPost("Register")]
-        public async Task<IActionResult> Register([FromBody] UserRegisterDto userLoginDto)
-        {
-                var registerResult = await _accountService.Register(userLoginDto);
+		}
 
-                if (registerResult != null)
-                {
-                    return Ok(registerResult);
-                }
+		[HttpPost("Register")]
+		public async Task<IActionResult> Register([FromBody] UserRegisterDto userLoginDto)
+		{
+			var registerResult = await _accountService.Register(userLoginDto);
 
-                return StatusCode(500, "Internal server error");
-        }
+			if (registerResult != null)
+			{
+				return Ok(registerResult);
+			}
 
-        [HttpGet("GetUserDataById/{userId:guid}")]
-        public async Task<IActionResult> GetUserDataById(Guid userId) {
-            try
-            {
-                var user = await _accountService.GetUserData(userId);
-                if (user == null)
-                {
-                    return NotFound();
-                }
+			return StatusCode(500, "Internal server error");
+		}
 
-                return Ok(_mapper.Map<ProfileDto>(user));
-            }
-            catch (Exception)
-            {
-                return StatusCode(500, "Internal server error");
-            }
-        }
+		[HttpGet("GetUserDataById/{userId:guid}")]
+		public async Task<IActionResult> GetUserDataById(Guid userId)
+		{
+			try
+			{
+				var user = await _accountService.GetUserData(userId);
+				if (user == null)
+				{
+					return NotFound();
+				}
 
-        [HttpPost("EditUserData")]
-        public async Task<IActionResult> EditUserData([FromBody] ProfileEditDto profileEditDto)
-        {
-            try
-            {
-                var editResult = await _accountService.EditUserData(profileEditDto);
-                if (editResult != null)
-                {
-                    return Ok(editResult);
-                }
+				return Ok(_mapper.Map<ProfileDto>(user));
+			}
+			catch (Exception)
+			{
+				return StatusCode(500, "Internal server error");
+			}
+		}
 
-                return StatusCode(500, "Internal server error");
-            }
-            catch (Exception)
-            {
-                return StatusCode(500, "Internal server error");
-            }
-        }
-    }
+		[HttpPost("EditUserData")]
+		public async Task<IActionResult> EditUserData([FromBody] ProfileEditDto profileEditDto)
+		{
+			try
+			{
+				var editResult = await _accountService.EditUserData(profileEditDto);
+				if (editResult != null)
+				{
+					return Ok(editResult);
+				}
+
+				return StatusCode(500, "Internal server error");
+			}
+			catch (Exception)
+			{
+				return StatusCode(500, "Internal server error");
+			}
+		}
+
+		[HttpPost("Logout")]
+		public async Task<IActionResult> Logout()
+		{
+			await _accountService.Logout();
+			return Ok();
+		}
+
+	}
 }
--- StudioAPI/StudioService/LoginService/Imp/AccountService.cs
+++ StudioAPI/StudioService/LoginService/Imp/AccountService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.V4.Pages.Account.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using StudioModel.Domain;
@@ -7,143 +8,150 @@ using StudioModel.Dtos.User;
 
 namespace StudioService.LoginService.Imp
 {
-    public class AccountService : IAccountService
-    {
-        private readonly SignInManager<UserApp> _signInManager;
-        private readonly UserManager<UserApp> _userManager;
-        private readonly ILogger<AccountService> _logger;
-        private readonly IJwtService _jwtService;
+	public class AccountService : IAccountService
+	{
+		private readonly SignInManager<UserApp> _signInManager;
+		private readonly UserManager<UserApp> _userManager;
+		private readonly ILogger<AccountService> _logger;
+		private readonly IJwtService _jwtService;
 
-        public AccountService(SignInManager<UserApp> signInManager, UserManager<UserApp> userManager, ILogger<AccountService> logger, IJwtService jwtService)
-        {
-            _signInManager = signInManager;
-            _userManager = userManager;
-            _logger = logger;
-            _jwtService = jwtService;
-        }
+		public AccountService(SignInManager<UserApp> signInManager, UserManager<UserApp> userManager, ILogger<AccountService> logger, IJwtService jwtService)
+		{
+			_signInManager = signInManager;
+			_userManager = userManager;
+			_logger = logger;
+			_jwtService = jwtService;
+		}
 
-        public async Task<UserToken?> Login(UserLoginDto userLoginDto)
-        {
-            try
-            {
-                _logger.LogTrace("Login begins");
+		public async Task<UserToken?> Login(UserLoginDto userLoginDto)
+		{
+			try
+			{
+				_logger.LogTrace("Login begins");
 
-                var result =
-                    await _signInManager.PasswordSignInAsync(userLoginDto.UserName, userLoginDto.Password, false, false);
+				var result =
+					await _signInManager.PasswordSignInAsync(userLoginDto.UserName, userLoginDto.Password, false, false);
 
-                if (result.Succeeded)
-                {
-                    var userApp = await _userManager.FindByEmailAsync(userLoginDto.UserName);
-                    var roles = await _userManager.GetRolesAsync(userApp!);
-                    userApp!.Role = roles.FirstOrDefault()!;
+				if (result.Succeeded)
+				{
+					var userApp = await _userManager.FindByEmailAsync(userLoginDto.UserName);
+					var roles = await _userManager.GetRolesAsync(userApp!);
+					userApp!.Role = roles.FirstOrDefault()!;
 
-                    var userToken = _jwtService.GeneratedToken(userApp);
+					var userToken = _jwtService.GeneratedToken(userApp);
 
-                    return userToken;
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Login error");
-            }
-            return null;
-        }
+					return userToken;
+				}
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex, "Login error");
+			}
+			return null;
+		}
 
-        public async Task<UserToken?> Register(UserRegisterDto userLoginDto)
-        {
-            try
-            {
-                if (userLoginDto.Password == userLoginDto.ConfirmPassword)
-                {
-                    var user = new UserApp()
-                    {
-                        CustomUserName = userLoginDto.UserName,
-                        Email = userLoginDto.Email,
-                        UserName = userLoginDto.Email,
-                        Birthday = userLoginDto.Birthdate,
-                    };
+		public async Task<UserToken?> Register(UserRegisterDto userLoginDto)
+		{
+			try
+			{
+				if (userLoginDto.Password == userLoginDto.ConfirmPassword)
+				{
+					var user = new UserApp()
+					{
+						CustomUserName = userLoginDto.UserName,
+						Email = userLoginDto.Email,
+						UserName = userLoginDto.Email,
+						Birthday = userLoginDto.Birthdate,
+					};
 
-                    var createResult = await _userManager.CreateAsync(user, userLoginDto.Password);
-                    if (createResult.Succeeded)
-                    {
-                        var addRolResult = await _userManager.AddToRoleAsync(user, "User");
-                        if (addRolResult.Succeeded)
-                        {
-                            var userLoged = new UserLoginDto()
-                            {
-                                UserName = userLoginDto.Email,
-                                Password = userLoginDto.Password,
-                            };
-                            return await Login(userLoged);
-                        }
-                    }
-                    else
-                    {
-                        var error = string.Join("*", createResult.Errors.Select(e => e.Description));
-                        _logger.LogError(error, "Registration error");
-                        return null;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Registration error");
-            }
+					var createResult = await _userManager.CreateAsync(user, userLoginDto.Password);
+					if (createResult.Succeeded)
+					{
+						var addRolResult = await _userManager.AddToRoleAsync(user, "User");
+						if (addRolResult.Succeeded)
+						{
+							var userLoged = new UserLoginDto()
+							{
+								UserName = userLoginDto.Email,
+								Password = userLoginDto.Password,
+							};
+							return await Login(userLoged);
+						}
+					}
+					else
+					{
+						var error = string.Join("*", createResult.Errors.Select(e => e.Description));
+						_logger.LogError(error, "Registration error");
+						return null;
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex, "Registration error");
+			}
 
-            return null;
-        }
+			return null;
+		}
 
-        public async Task<UserApp?> GetUserData(Guid userId)
-        {
-            try
-            {
-                var user = await _userManager.FindByIdAsync(userId.ToString());
-                if (user != null)
-                {
-                    return user;
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "GetUserData error");
-            }
+		public async Task<UserApp?> GetUserData(Guid userId)
+		{
+			try
+			{
+				var user = await _userManager.FindByIdAsync(userId.ToString());
+				if (user != null)
+				{
+					return user;
+				}
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex, "GetUserData error");
+			}
 
-            _logger.LogError("User not found");
-            return null;
-        }
+			_logger.LogError("User not found");
+			return null;
+		}
 
-        public async Task<UserToken?> EditUserData(ProfileEditDto profileEditDto)
-        {
-            try
-            {
-                var user = await _userManager.FindByIdAsync(profileEditDto.idUser);
-                if (user != null)
-                {
-                    user.UserName = profileEditDto.userProfile.UserName;
-                    user.CustomUserName = profileEditDto.userProfile.CustomUserName;
-                    user.Birthday = profileEditDto.userProfile.Birthday;
-                    user.PhoneNumber = profileEditDto.userProfile.PhoneNumber;
-                    user.UserPhoto = profileEditDto.userProfile.UserPhoto;
+		public async Task<UserToken?> EditUserData(ProfileEditDto profileEditDto)
+		{
+			try
+			{
+				var user = await _userManager.FindByIdAsync(profileEditDto.idUser);
+				if (user != null)
+				{
+					user.UserName = profileEditDto.userProfile.UserName;
+					user.CustomUserName = profileEditDto.userProfile.CustomUserName;
+					user.Birthday = profileEditDto.userProfile.Birthday;
+					user.PhoneNumber = profileEditDto.userProfile.PhoneNumber;
+					user.UserPhoto = profileEditDto.userProfile.UserPhoto;
 
-                    var updateResult = await _userManager.UpdateAsync(user);
-                    if (updateResult.Succeeded)
-                    {
-                        var roles = await _userManager.GetRolesAsync(user!);
-                        user!.Role = roles.FirstOrDefault()!;
+					var updateResult = await _userManager.UpdateAsync(user);
+					if (updateResult.Succeeded)
+					{
+						var roles = await _userManager.GetRolesAsync(user!);
+						user!.Role = roles.FirstOrDefault()!;
 
-                        var userToken = _jwtService.GeneratedToken(user);
+						var userToken = _jwtService.GeneratedToken(user);
 
-                        return userToken;
-                    }
-                }
+						return userToken;
+					}
+				}
 
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "EditUserData error");
-            }
-            _logger.LogError("User not found");
-            return null;
-        }
-    }
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex, "EditUserData error");
+			}
+			_logger.LogError("User not found");
+			return null;
+		}
+
+		public async Task Logout()
+		{
+			await _signInManager.SignOutAsync();
+
+			_logger.LogInformation("Logout successful");
+		}
+	}
 }
