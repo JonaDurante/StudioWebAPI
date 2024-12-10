7f7ee13b1fe1ffe446684edd2baaf5baaded09f4
nkonko <nicoazzara@hotmail.com>
2024-12-11 00:14:11 +0100

    added some tests to setup runner

---
+++ StudioAPI/Tests/AccountServiceTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Moq;
+using StudioModel.Domain;
+using StudioModel.Dtos.Account;
+using StudioService.Services;
+using StudioService.Services.Imp;
+using Xunit;
+
+
+/// CHECK HOW TO CONFIGURE SIGN IN MANAGER
+namespace StudioBack.Tests
+{
+    //public class AccountServiceTests
+    //{
+    //    private AccountService _accountService;
+    //    private Mock<SignInManager<UserApp>> _signInManager;
+    //    private Mock<UserManager<UserApp>> _userManager;
+    //    private Mock<ILogger<AccountService>> _logger;
+    //    private Mock<IJwtService> _jwtService;
+    //    private Mock<IEmailService> _emailService;
+
+    //    public AccountServiceTests()
+    //    {
+    //        Setup();
+    //        _accountService = new AccountService(_signInManager.Object, _userManager.Object, _logger.Object, _jwtService.Object, _emailService.Object);
+    //    }
+
+    //    public void Setup()
+    //    {
+    //        var mockUserStore = new Mock<IUserStore<UserApp>>();
+    //        var mockUserManager = new Mock<UserManager<UserApp>>(mockUserStore.Object);
+    //        _signInManager = new Mock<SignInManager<UserApp>>(mockUserManager.Object);
+    //        _userManager = mockUserManager;
+    //        _logger = new Mock<ILogger<AccountService>>();
+    //        _jwtService = new Mock<IJwtService>();
+    //        _emailService = new Mock<IEmailService>();
+
+    //        var mockedUserApp = getMockedUserApp();
+    //        this._userManager.Setup(sm => sm.FindByEmailAsync(It.IsAny<string>())).ReturnsAsync(mockedUserApp);
+    //    }
+
+
+    //    [Fact]
+    //    public async Task Login()
+    //    {
+    //        var mockedDto = new UserLoginDto { Email = "nico@test.com", Password = "1234" };
+    //        var result = await _accountService.Login(mockedDto);
+
+    //        result.Should().NotBeNull();
+    //    }
+
+
+    //    private UserApp getMockedUserApp()
+    //    {
+    //        return new UserApp()
+    //        {
+    //            Id = "1",
+    //            Email = "nico@test.com",
+    //        };
+    //    }
+    //}
+}
---
+++ StudioAPI/Tests/EnrollmentServiceTests.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Moq;
+using StudioDataAccess.Repositories;
+using StudioDataAccess.Uow;
+using StudioModel.Domain;
+using StudioModel.Dtos.Enrollment;
+using StudioService.Services.Imp;
+using Xunit;
+
+namespace StudioBack.Tests
+{
+    public class EnrollmentServiceTests
+    {
+        private Mock<IUnitOfWork> _unitOfWorkMock;
+        private Mock<IMapper> _mapperMock;
+        private EnrollmentService _enrollmentService;
+
+        public EnrollmentServiceTests()
+        {
+            Setup();
+            _enrollmentService = new EnrollmentService(_unitOfWorkMock.Object, _mapperMock.Object);
+        }
+
+        private void Setup()
+        {
+            var mockCourseRepository = new Mock<ICourseRepository>();
+            mockCourseRepository.Setup(c => c.GetById(It.IsAny<Guid>())).ReturnsAsync(new Course()
+            {
+                Id = Guid.NewGuid()
+            });
+            _unitOfWorkMock = new Mock<IUnitOfWork>();
+            _unitOfWorkMock.Setup(uow => uow.CourseRepository).Returns(mockCourseRepository.Object);
+            _mapperMock = new Mock<IMapper>();
+        }
+
+        [Fact]
+        public async Task EnrollUserTest()
+        {
+            var mockedDto = new EnrollmentDto()
+            {
+                CourseId = Guid.NewGuid(),
+                EnrollmentDate = DateTime.UtcNow.AddDays(1),
+                UserId = Guid.NewGuid().ToString(),
+            };
+
+            var result = await _enrollmentService.EnrollUser(mockedDto);
+        }
+
+        [Fact]
+        public async Task EnrollUserTestFailOnEnrollmentDay()
+        {
+            var mockedDto = new EnrollmentDto()
+            {
+                CourseId = Guid.NewGuid(),
+                EnrollmentDate = DateTime.UtcNow.AddDays(-10),
+                UserId = Guid.NewGuid().ToString(),
+            };
+
+            Func<Task> act = async () => await _enrollmentService.EnrollUser(mockedDto);
+
+            await act.Should().ThrowAsync<Exception>().WithMessage("La fecha debe ser mayor al dia de hoy");
+        }
+    }
+}
